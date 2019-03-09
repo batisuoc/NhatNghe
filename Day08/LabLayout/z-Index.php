92a6b7433843cee2109beb6ae6e0f11b5bd35cea
batisuoc <zhenghengyue@gmail.com>
2019-03-09 23:18:12 +0700

    First commit

---
+++ Day08/LabLayout/z-Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style>
+#a { width:400px; height:300px; background-color:#933; z-index:5; position:relative}
+#b { width:400px; height:300px; background-color:#C60; position:relative; top:-100px; left:50px; z-index:3;}
+</style>
+</head>
+
+<body>
+<div id="a">aa</div>
+<div  id="b">bb</div>
+</body>
+</html>
