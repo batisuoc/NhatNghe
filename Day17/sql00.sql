819788e2181dac1d284136e59ba3a9867b658684
batisuoc <zhenghengyue@gmail.com>
2019-03-14 21:05:30 +0700

    Review SQL and learn ERD

---
+++ Day17/sql00.sql
@@ -0,0 +1,10 @@
+update khachhang
+set DiaChi = 'Q10'
+where Username = 'aa';
+
+update khachhang
+set DienThoai = '093445212'
+where Username = 'teonv';
+
+delete from khachhang
+where Email = 'b';
